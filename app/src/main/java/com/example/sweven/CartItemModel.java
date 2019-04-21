04dd83aa03ea5d05d23c78bddc956952072f3065
Parth Maniar <maniarparth316@gmail.com>
2019-04-21 11:19:41 +0530

    Initial Commit2

---
+++ app/src/main/java/com/example/sweven/CartItemModel.java
@@ -0,0 +1,24 @@
+package com.example.sweven;
+
+public class CartItemModel {
+
+    public static final int CART_ITEM = 0;
+    public static final int TOTAL_AMOUNT = 1;
+
+    private int type;
+    /* **************************CART ITEM************************** */
+
+    private int productImage;
+    private String productTitle;
+    private int freeCoupons;
+    private String productPrice;
+    private String cuttedPrice;
+    private int productQuantity;
+    private int offersApplied;
+    private int couponsApplied;
+
+
+
+
+    /* **************************CART ITEM************************** */
+}
